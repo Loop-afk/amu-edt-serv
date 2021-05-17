17892a348c06cd855bc89f09226766faa25df5e1
aaa <baptiste.souche@etu.univ-amu.fr>
2021-05-17 18:15:45 +0000

    broken connection refused

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const mysql = require('mysql');
 
 module.exports = router;
 
@@ -8,6 +9,24 @@ module.exports = router;
 
 router
     .get("/", (req, res) => {
+        const con = mysql.createConnection({
+        host: "77.145.66.7",
+        port: "18931",
+        user: "root",
+        password: "amuedtmdp",
+        database : "amu_edt"
+        });
+
+        con.connect(function(err) {
+        if (err) {throw err;}
+        console.log("Connecté à la base de données MySQL!");
+        con.query("SELECT * FROM Users", function (err, result) {
+            if (err) {throw err;}
+            console.log(result);
+            res.json(result)
+            });
+        });
+
        res.json("Hello world!!");
    });
    
