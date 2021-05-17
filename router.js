032c034cc33f4460491b4dd87c21617a8bd7a764
Aznastieme <volamigaming13@gmail.com>
2021-05-17 22:48:52 +0200

    Connexion serveur + requete console

--- router.js
+++ router.js
@@ -1,27 +1,8 @@
 const express = require("express");
-const data = require('./db/data');
 const router = express.Router();
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
-
-const mysql = require('mysql');
-
-const con = mysql.createConnection({
-   host: "http://77.145.66.7:18932/",
-   user: "root",
-   password: "fqm28bdh"
- });
-
-  con.connect(function(err) {
-   if (err) throw err;
-   console.log("Connecté à la base de données MySQL!");
-   con.query("SELECT userFirstName from User", function (err, result) {
-       if (err) throw err;
-       console.log(result);
-     });
- });
-
- module.exports = router;
+module.exports = router;
 
 
 router
@@ -37,109 +18,12 @@ router
         con.connect(function(err) {
         if (err) {throw err;}
         console.log("Connecté à la base de données MySQL!");
-        con.query("SELECT * FROM Users", function (err, result) {
+        con.query("SELECT * FROM User where userFirstName = 'Mickael'", function (err, result) {
             if (err) {throw err;}
             console.log(result);
-            res.json(result)
             });
         });
 
        res.json("Hello world!!");
    });
-
-
-router
-.get("/test", (req, res) => {
-    res.json([
-        {
-          "day": {
-            "day": 20,
-            "month": 5,
-            "year": 2021
-          },
-          "groups": [
-            {
-              "id": 2,
-              "value": "L3 info"
-            }
-          ],
-          "start": {
-            "hours": 7,
-            "minutes": 45
-          },
-          "end": {
-            "hours": 10,
-            "minutes": 0
-          },
-          "ue": {
-            "field": {
-              "id": 1,
-              "value": "Fourth Event"
-            },
-            "date": {
-              "year": 2020,
-              "semester": 0
-            }
-          },
-          "teacher": {
-            "id": 1,
-            "value": "Nobody."
-          },
-          "place": {
-            "room": {
-              "id": 1,
-              "value": "Home"
-            },
-            "campus": {
-              "id": 1,
-              "value": "Luminy"
-            }
-          }
-        },
-        {
-          "day": {
-            "day": 14,
-            "month": 5,
-            "year": 2021
-          },
-          "groups": [
-            {
-              "id": 1,
-              "value": "L3 info"
-            }
-          ],
-          "start": {
-            "hours": 6,
-            "minutes": 0
-          },
-          "end": {
-            "hours": 28,
-            "minutes": 0
-          },
-          "ue": {
-            "field": {
-              "id": 1,
-              "value": "Third Event"
-            },
-            "date": {
-              "year": 2020,
-              "semester": 0
-            }
-          },
-          "teacher": {
-            "id": 1,
-            "value": "Nobody."
-          },
-          "place": {
-            "room": {
-              "id": 1,
-              "value": "Home"
-            },
-            "campus": {
-              "id": 1,
-              "value": "Luminy"
-            }
-          }
-        }
-      ]);
-});
+   
