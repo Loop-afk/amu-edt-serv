bd8254b612008ca07b896fe30506aea884ae34c9
Aznastieme <volamigaming13@gmail.com>
2021-05-19 20:40:22 +0200

    Format requete SQL complete entre from et to

--- router.js
+++ router.js
@@ -14,6 +14,7 @@ const con = mysql.createConnection({
 module.exports = router;
 
 /* http://192.168.1.29:8000/?userFirstName=Mickael */
+///schedule/?from=2021-04-10&to=2021-04-20
 
 /*router
     .get("/", (req, res) => {
@@ -29,7 +30,35 @@ module.exports = router;
         });
    });*/
 
-   router
+//    /?from=2021-04-10&to=2021-04-20
+
+router
+    .get("/", (req, res) => {
+        let from = req.query.from
+        let to = req.query.to
+        console.log(from, to)
+        con.connect(function(err) {
+        if (err) {throw err;}
+        //console.log("Connecté à la base de données MySQL!");
+        con.query(`SELECT EXTRACT(YEAR FROM courseDate) AS Year, EXTRACT(MONTH FROM courseDate) AS Month, EXTRACT(DAY FROM courseDate) as Day,
+                    C.gradeId, G.gradeName, 
+                    EXTRACT(HOUR FROM courseStart) as hourStart, EXTRACT(MINUTE FROM courseStart) as minuteStart,
+                    EXTRACT(HOUR FROM courseEnd) as hourEnd, EXTRACT(MINUTE FROM courseEnd) as minuteEnd,
+                    C.subjectId, S.subjectName,
+                    C.teacherId, U.userFirstName as teacherfirstName, U.userLastName as teacherLastName,
+                    C.roomId, R.roomName, Ca.campusId, Ca.campusName
+                    FROM Course C INNER JOIN Grade G on C.gradeId = G.gradeId INNER JOIN Subject S on C.subjectId = S.subjectId
+                    INNER JOIN User U on C.teacherId = U.userId INNER JOIN Room R on C.roomId = R.roomId INNER JOIN Campus Ca on R.campusId = Ca.campusId
+                    WHERE courseDate <= '${to}' AND courseDate >=  '${from}'`, function (err, result) {
+            if (err) {throw err;}
+            //console.log(result);
+            res.json(result);
+        });
+        });
+        
+   });
+
+   /*router
    .get("/", (req, res) => {
         let from = req.query.from
         let fromSplit = from.split('-')
@@ -133,7 +162,7 @@ module.exports = router;
                 return course.date.day >= fromSplit[2] && course.date.day <= toSplit[2] && course.date.month >= fromSplit[1] && course.date.month <= toSplit[1] && course.date.year >= fromSplit[0] && course.date.year <= toSplit[0];
                       });
         res.json(inFromTo);
-   });
+   });*/
    
 
 
